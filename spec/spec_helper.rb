3d4c9e8757d544fd4463127a048b5f2ec5ae2253
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-01-14 14:34:16 -0200

    Adding codeclimate code into spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ require 'simple_google_drive'
 
 require 'webmock/rspec'
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
