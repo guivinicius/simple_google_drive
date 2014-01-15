0f1d8e655199f35407b8f62574d75d2908c6b8a3
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-01-15 16:50:49 -0200

    Fixing codecliamte communication problem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ CodeClimate::TestReporter.start
 require 'simple_google_drive'
 require 'webmock/rspec'
 
+# Allowing codeclimate connections
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
